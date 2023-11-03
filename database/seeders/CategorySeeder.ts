ab051a1104ea6c3226bf3a48fa464556d108dd4b
ridwanalfarezi <alfareziridwan@gmail.com>
2023-11-03 17:11:09 +0700

    initial commit

---
+++ database/seeders/CategorySeeder.ts
@@ -0,0 +1,15 @@
+import BaseSeeder from '@ioc:Adonis/Lucid/Seeder'
+import Category from 'App/Models/Category'
+
+export default class extends BaseSeeder {
+  public async run() {
+    // Write your database queries inside the run method
+    await Category.createMany([
+      { title: 'Food' },
+      { title: 'Travel' },
+      { title: 'Entertainment' },
+      { title: 'Gamer' },
+      { title: 'Technology' },
+    ])
+  }
+}
